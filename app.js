499aef981f10dae34fc1bc468020bc7528d7bf48
InjamulRashik <ihrashik@gmail.com>
2021-08-17 11:35:20 +0600

    Mongo DB Connected!

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const { MONGOURI } = require("./keys");
 const PORT = 5000;
 
-app.get("/", (req, res) => {
+const customMiddleware = (req, res, next) => {
+  next();
+};
+
+mongoose.connect(MONGOURI);
+mongoose.connection.on("connected", () => {
+  console.log("Conection with Mongo Done!");
+});
+
+app.use(customMiddleware);
+
+app.get("/", customMiddleware, (req, res) => {
+  console.log("Check");
   res.send("Working");
 });
 
---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://ihrashik:mihr001100@cluster0.4xgzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
